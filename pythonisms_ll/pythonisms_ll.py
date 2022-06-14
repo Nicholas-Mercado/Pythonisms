45e461cb2ad723769a025c048dca9946ffe8f521
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-06-14 15:24:45 -0700

    can list comprehension

--- pythonisms_ll/pythonisms_ll.py
+++ pythonisms_ll/pythonisms_ll.py
@@ -1,6 +1,13 @@
 class LinkedList:
-    def __init__(self):
+    '''
+    Methods:
+    insert --> adds node to front of list
+    '''
+    def __init__(self, collection=None):
         self.head = None
+        if collection:
+            for item in reversed(collection):
+                self.insert(item)
 
     def insert(self, value):
         current = self.head
@@ -17,9 +24,22 @@ class LinkedList:
             current = current.next
         print(output + null)
         return output + null
+    
+    def __iter__(self):
+        
+        def generator():
+            
+            current = self.head
+            while current:
+                yield current.value
+                current = current.next
+
+        return generator()
 
 
 class Node:
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
+
+
--- tests/tests_pythonisms.py
+++ tests/tests_pythonisms.py
@@ -28,3 +28,56 @@ def test_to_string_empty():
 
     assert str(linked_list) == "NULL"
 
+# add ability to be used in a for in loop
+
+def test_for_in():
+    
+    ll = LinkedList()
+    ll.insert("apple")
+    ll.insert("apple2")
+    ll.insert("apple3")
+    ll.insert("apple4")
+    
+    apple_list = []
+    
+    for link in ll:
+        apple_list.append(link)
+        
+    assert apple_list == ["apple4","apple3","apple2","apple"]
+    
+def test_for_in_then_do_something():
+    
+    ll = LinkedList()
+    ll.insert("apple")
+    ll.insert("apple2")
+    ll.insert("apple3")
+    ll.insert("apple4")
+    
+    apple_list = []
+    
+    for link in ll:
+        link = link.upper()
+        apple_list.append(link)
+        
+    assert apple_list == ['APPLE4', 'APPLE3', 'APPLE2', 'APPLE']
+# add ability to be used in a list comprehension
+
+def test_list_comprehension():
+    
+    ll = LinkedList(("apple4","apple3","apple2","apple"))
+    
+    upper_ll = [link.upper() for link in ll]
+    
+    assert upper_ll == ['APPLE4', 'APPLE3', 'APPLE2', 'APPLE']
+    
+
+
+# add ability to convert to a list or other collection type
+
+def test_list__can_cast():
+
+    food_list = ["apple","banana","cucumber"]
+
+    foods = LinkedList(food_list)
+
+    assert list(foods) == food_list
