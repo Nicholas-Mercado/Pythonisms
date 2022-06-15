58f2bc8826a1f895d5e9588c6a195c89e20b4da2
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-06-14 17:40:39 -0700

    can time functions

--- pythonisms/decorators.py
+++ pythonisms/decorators.py
@@ -1,20 +1,41 @@
 from functools import wraps
+from time import time
+
 
 def exclamation(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        
+
         un_changed_function_return = func(*args, **kwargs)
-        
+
         exclamation = un_changed_function_return + "!!!!!"
-        
+
         return exclamation
-    
+
     return wrapper
+
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        t = time()
+        result = func(*args, **kwargs)
+        t2 = time()
+        print(f"Function : {func.__name__!r} finished in {(t2-t):.4f}s")
+        return result
+    return wrapper
+
 @exclamation
+@timer
 def say(txt):
     return txt
 
+@timer
+def long_function():
+    for x in range(1,101):
+        for y in range(1,500):
+            print(x)
+
 
 if __name__ == "__main__":
-    print(say("Here we Go!"))
+    print(long_function())
